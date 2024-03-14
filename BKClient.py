496a1a9d9c311373d176088f77971fff79e7a50a
llmexplorer <anonymous@email.com>
2024-03-13 22:02:36 -0700

    A script that can scrape Burger King's API for restaurants, menu items, pricing data, and item details.

---
+++ BKClient.py
@@ -0,0 +1,322 @@
+import requests
+import json
+from concurrent.futures import ThreadPoolExecutor
+import os
+from collections import namedtuple
+
+ItemInfo = namedtuple("ItemInfo", ["id", "name", "image_url", "nutrition", "is_dummy", "category"])
+
+
+class BKClient:
+    
+    def __init__(self):
+        
+        self.menu_template = """https://use1-prod-bk-gateway.rbictg.com/graphql?operationName=storeMenu&variables=%%7B%%22channel%%22%%3A%%22whitelabel%%22%%2C%%22region%%22%%3A%%22US%%22%%2C%%22storeId%%22%%3A%%22%s%%22%%2C%%22serviceMode%%22%%3A%%22pickup%%22%%7D&extensions=%%7B%%22persistedQuery%%22%%3A%%7B%%22version%%22%%3A1%%2C%%22sha256Hash%%22%%3A%%2248a3fa9cd76ee8e29027ab0d4d13bf5bfb1eca856f312735fa572a2c3acec90b%%22%%7D%%7D"""
+        self.nearby_store_template = """https://use1-prod-bk-gateway.rbictg.com/graphql?operationName=GetNearbyRestaurants&variables=%%7B%%22input%%22%%3A%%7B%%22pagination%%22%%3A%%7B%%22first%%22%%3A100%%7D%%2C%%22radiusStrictMode%%22%%3Afalse%%2C%%22coordinates%%22%%3A%%7B%%22searchRadius%%22%%3A10000000%%2C%%22userLat%%22%%3A%s%%2C%%22userLng%%22%%3A%s%%7D%%7D%%7D&extensions=%%7B%%22persistedQuery%%22%%3A%%7B%%22version%%22%%3A1%%2C%%22sha256Hash%%22%%3A%%221d288d2ae206ab197a3a9aff0d7cf8997b2842cbe21dea7fac94cc8a92acdb43%%22%%7D%%7D"""
+        self.store_info_template = """https://czqk28jt.apicdn.sanity.io/v1/graphql/prod_bk_us/default?operationName=GetRestaurants&variables=%%7B%%22filter%%22%%3A%%7B%%22_id%%22%%3A%%22%s%%22%%7D%%2C%%22limit%%22%%3A1%%7D&query=query+GetRestaurants%%28%%24filter%%3ARestaurantFilter%%24limit%%3AInt%%29%%7BallRestaurants%%28where%%3A%%24filter+limit%%3A%%24limit%%29%%7B...RestaurantFragment+__typename%%7D%%7Dfragment+RestaurantFragment+on+Restaurant%%7B_id+environment+chaseMerchantId+deliveryHours%%7B...HoursFragment+__typename%%7DdiningRoomHours%%7B...HoursFragment+__typename%%7DcurbsideHours%%7B...HoursFragment+__typename%%7DdriveThruHours%%7B...HoursFragment+__typename%%7DdrinkStationType+driveThruLaneType+email+fastestServiceMode+franchiseGroupId+franchiseGroupName+frontCounterClosed+hasBreakfast+hasBurgersForBreakfast+hasCurbside+hasDineIn+hasCatering+hasDelivery+hasDriveThru+hasMobileOrdering+hasParking+hasPlayground+hasTakeOut+hasWifi+hasLoyalty+isDarkKitchen+isHalal+latitude+longitude+mobileOrderingStatus+name+number+parkingType+phoneNumber+playgroundType+pos%%7B_type+vendor+__typename%%7DphysicalAddress%%7B_type+address1+address2+city+country+postalCode+stateProvince+__typename%%7DposRestaurantId+restaurantPosData%%7B_id+__typename%%7Dstatus+restaurantImage%%7Basset%%7B...ImageAssetFragment+__typename%%7D__typename%%7Damenities%%7Bname%%7Blocale%%3Aen+__typename%%7Dicon%%7Basset%%7B...ImageAssetFragment+__typename%%7D__typename%%7D__typename%%7Dtimezone+vatNumber+__typename%%7Dfragment+HoursFragment+on+HoursOfOperation%%7B_type+friClose+friOpen+monClose+monOpen+satClose+satOpen+sunClose+sunOpen+thrClose+thrOpen+tueClose+tueOpen+wedClose+wedOpen+__typename%%7Dfragment+ImageAssetFragment+on+SanityImageAsset%%7B_id+label+title+url+source%%7Bid+url+__typename%%7Dmetadata%%7BblurHash+__typename%%7D__typename%%7D"""
+
+        self.item_info_template = """https://czqk28jt.apicdn.sanity.io/v1/graphql/prod_bk_us/default?operationName=GetPicker&variables=%7B%22id%22%3A%22{item_id}%22%7D&query={query}"""
+
+        # load the item info query from a file
+        with open(f"Queries{os.sep}ItemInfo.gql", "r") as f:
+            self.item_info_query = f.read()
+
+
+    def any_not_in(self, d, key_list):
+        """
+        Checks if the hierarchy of keys in key_list exists in the dictionary d.
+
+        Args:
+            d (dict): The dictionary to check.
+            key_list (list): A list of keys that form the hierarchy to check.
+
+        Returns:
+            bool: True if the hierarchy of keys exists in the dictionary, False otherwise.
+        """
+
+        for key in key_list:
+            if not d or key not in d:
+                return True
+            d = d[key]
+
+        return False
+
+
+    def get_menu(self, store_id, session=None):
+        """
+        Fetches the menu for a Burger King store.
+
+        Args:
+            store_id (str): The store ID for which the menu needs to be fetched.
+            session (requests.Session, optional): A requests session object to use for the request. Defaults to None.
+
+        Returns:
+            dict: The menu for the specified store, or None if the menu cannot be fetched.
+        """
+
+        url = self.menu_template % store_id
+
+        if session:
+            resp = session.get(url)
+        else:
+            resp = requests.get(url)
+
+        if resp.status_code == 200:
+            j = resp.json()
+
+        if 'data' in j and 'storeMenu' in j['data']:
+            return j['data']['storeMenu']
+        
+
+    def get_many_menus(self, menus, threads=1):
+        """
+        Fetches menus for multiple Burger King stores concurrently.
+
+        Args:
+            menus (list): A list of store IDs for which the menus need to be fetched.
+            threads (int, optional): The number of threads to use for concurrent requests. Defaults to 1.
+
+        Returns:
+            dict: A dictionary mapping store IDs to their corresponding menus. If a menu cannot be fetched for a store (due to an invalid store ID or a failed request), the store ID will not be included in the returned dictionary.
+        """
+
+        results = {}
+        
+        with ThreadPoolExecutor(max_workers=threads) as executor:
+            session = requests.Session()
+            futures = {store_id: executor.submit(self.get_menu, store_id, session) for store_id in menus}
+
+            for store_id, future in futures.items():
+                result = future.result()
+                if result:
+                    results[store_id] = result
+
+        return results
+    
+
+    def get_nearby_stores(self, lat, lon, session=None, ids_only=False):
+        """
+        Fetches nearby Burger King stores based on latitude and longitude.
+
+        Args:
+            lat (float): The latitude of the location.
+            lon (float): The longitude of the location.
+            session (requests.Session, optional): A requests session object to use for the request. Defaults to None.
+            ids_only (bool, optional): If True, only store IDs will be returned. If False, store information dictionaries will be returned. Defaults to False.
+
+        Returns:
+            list: A list of store IDs if ids_only is True, or a list of store information dictionaries if ids_only is False. Returns an empty list if no stores are found.
+        """
+
+        url = self.nearby_store_template % (lat, lon)
+
+        if session:
+            resp = session.get(url)
+        else:
+            resp = requests.get(url)
+
+        if resp.status_code == 200:
+            j = resp.json()
+
+        if self.any_not_in(j, ['data', 'restaurantsV2', 'nearby', 'nodes']):
+            return []
+        
+        if ids_only:
+            return [store['storeId'] for store in j['data']['restaurantsV2']['nearby']['nodes']]
+        else:
+            return j['data']['restaurantsV2']['nearby']['nodes']
+        
+
+    def get_many_nearby_stores(self, locations, threads=1):
+        """
+        Fetches nearby Burger King stores for multiple locations concurrently.
+
+        Args:
+            locations (list): A list of (latitude, longitude) tuples for which nearby stores need to be fetched.
+            threads (int, optional): The number of threads to use for concurrent requests. Defaults to 1.
+
+        Returns:
+            dict: A dictionary mapping store IDs to their corresponding store information. If information cannot be fetched for a store (due to an invalid store ID or a failed request), the store ID will not be included in the returned dictionary.
+        """
+
+        results = {}
+        
+        with ThreadPoolExecutor(max_workers=threads) as executor:
+            session = requests.Session()
+            futures = [executor.submit(self.get_nearby_stores, lat, lon, session) for lat, lon in locations]
+
+            for future in futures:
+                result = future.result()
+                if result:
+                    results.update({store['storeId']: store for store in result})
+
+        return results
+    
+    
+    def get_store_info(self, restaurant_id, session=None):
+        """
+        Fetches information about a Burger King store.
+
+        Args:
+            restaurant_id (str): The restaurant id (usually restaurant_somenumber) for which the information needs to be fetched.
+            session (requests.Session, optional): A requests session object to use for the request. Defaults to None.
+
+        Returns:
+            dict: Information about the specified store, or None if the information cannot be fetched.
+        """
+
+        url = self.store_info_template % restaurant_id
+
+        if session:
+            resp = session.get(url)
+        else:
+            resp = requests.get(url)
+
+        if resp.status_code == 200:
+            j = resp.json()
+
+        if "data" in j and "allRestaurants" in j["data"]:
+            return j["data"]["allRestaurants"][0]
+        
+
+    def get_many_store_info(self, restaurant_ids, threads=1):
+        """
+        Fetches information about multiple Burger King stores concurrently.
+
+        Args:
+            restaurant_ids (list): A list of restaurant IDs for which the information needs to be fetched.
+            threads (int, optional): The number of threads to use for concurrent requests. Defaults to 1.
+
+        Returns:
+            dict: A dictionary mapping restaurant IDs to their corresponding store information. If information cannot be fetched for a store (due to an invalid restaurant ID or a failed request), the restaurant ID will not be included in the returned dictionary.
+        """
+
+        results = {}
+        
+        with ThreadPoolExecutor(max_workers=threads) as executor:
+            session = requests.Session()
+            futures = {restaurant_id: executor.submit(self.get_store_info, restaurant_id, session) for restaurant_id in restaurant_ids}
+
+            for restaurant_id, future in futures.items():
+                result = future.result()
+                if result:
+                    results[restaurant_id] = result
+
+        return results
+    
+
+    def get_item_info(self, item_id, session=None):
+        """
+        Fetches information about a Burger King menu item.
+
+        Args:
+            item_id (str): The item ID for which the information needs to be fetched.
+            session (requests.Session, optional): A requests session object to use for the request. Defaults to None.
+
+        Returns:
+            ItemInfo: An ItemInfo named tuple containing information about the specified item, or None if the information cannot be fetched.
+        """
+
+        url = self.item_info_template.format(item_id=item_id, query=self.item_info_query)
+
+        if session:
+            resp = session.get(url)
+        else:
+            resp = requests.get(url)
+
+        if resp.status_code == 200:
+            j = resp.json()
+
+        if "data" in j and "Picker" in j["data"]:
+            options = j['data']['Picker'].get('options', [])
+            zero_option = options[0] if options else None
+
+            if not zero_option:
+                return None
+            
+            zero_option = options[0].get('option') if options else None
+
+            if zero_option is None:
+                return None
+
+            if 'name' not in zero_option or 'locale' not in zero_option['name']:
+                return None
+            
+            if 'image' not in zero_option or 'asset' not in zero_option['image']:
+                return None
+            
+            if 'nutrition' not in zero_option:
+                return None
+            
+            if 'isDummyItem' not in zero_option:
+                return None
+            
+            if self.any_not_in(zero_option, ['productHierarchy', 'L2']):
+                category = "Not categorized"
+            else:
+                category = j['data']['Picker']['options'][0]['option']['productHierarchy']['L2']
+
+            name = j['data']['Picker']['options'][0]['option']['name']['locale']
+            image_url = j['data']['Picker']['options'][0]['option']['image']['asset']['url']
+            nutrition = j['data']['Picker']['options'][0]['option']['nutrition']
+            is_dummy = j['data']['Picker']['options'][0]['option']['isDummyItem'] != None
+            
+
+            return ItemInfo(item_id, name, image_url, nutrition, is_dummy, category)
+    
+
+    def get_many_item_info(self, item_ids, threads=1):
+        """
+        Fetches information about multiple Burger King menu items concurrently.
+
+        Args:
+            item_ids (list): A list of item IDs for which the information needs to be fetched.
+            threads (int, optional): The number of threads to use for concurrent requests. Defaults to 1.
+
+        Returns:
+            dict: A dictionary mapping item IDs to their corresponding item information. If information cannot be fetched for an item (due to an invalid item ID or a failed request), the item ID will not be included in the returned dictionary.
+        """
+
+        results = {}
+        
+        with ThreadPoolExecutor(max_workers=threads) as executor:
+            session = requests.Session()
+            futures = {item_id: executor.submit(self.get_item_info, item_id, session) for item_id in item_ids}
+
+            for item_id, future in futures.items():
+                result = future.result()
+                if result:
+                    results[item_id] = result
+
+        return results
+    
+
+    def search_lat_lon(self, lat_start, lat_end, lon_start, lon_end, increment=0.5, ids_only=True):
+        """
+        Given starting and ending coordinates search the area for Burger King locations and return a list of store IDs if ids_only is True, otherwise a list of dictionary objects representing stores.
+
+        Args:
+            lat_start (float): The starting latitude of the search area.
+            lat_end (float): The ending latitude of the search area.
+            lon_start (float): The starting longitude of the search area.
+            lon_end (float): The ending longitude of the search area.
+
+        Returns:
+            list: A list of store IDs if ids_only is True, or a list of store information dictionaries if ids_only is False. Returns an empty list if no stores are found.
+        """
+
+        intersections = []
+
+        cur_lat = lat_start
+        cur_lon = lon_start
+
+        while cur_lat > lat_end:
+            while cur_lon < lon_end:
+                intersections.append((cur_lat, cur_lon))
+                cur_lon += increment
+            cur_lat -= increment
+            cur_lon = lon_start
+
+        bks = self.get_many_nearby_stores(intersections, threads=10)
+
+        return bks
---
+++ BKDataHarvest.py
@@ -0,0 +1,336 @@
+from BKClient import BKClient
+import time
+import csv
+import os
+
+bkc = BKClient()
+
+
+def search_usa():
+
+    contiguous_states = {
+        "lat_start": 49.384358,
+        "lat_end": 24.396308,
+        "lon_start": -124.848974,
+        "lon_end": -66.885444
+    }
+
+    hawaii = {
+        "lat_start": 22.533,
+        "lat_end": 18.709,
+        "lon_start": -160.950,
+        "lon_end": -154.490
+    }
+
+    alaska = {
+        "lat_start": 71.371,
+        "lat_end": 55.304,
+        "lon_start": -169.233,
+        "lon_end": -140.669
+    }
+
+    contiguous_states = bkc.search_lat_lon(**contiguous_states)
+    hawaii = bkc.search_lat_lon(**hawaii)
+    alaska = bkc.search_lat_lon(**alaska)
+
+    return {**contiguous_states, **hawaii, **alaska}
+
+
+def simple_menu_item(store_id, item):
+    """
+    If the item has a valid id, isAvailable, price, and calories, return a tuple of those values.  Otherwise, return None.
+
+    Args:
+        item (dict): A dictionary representing a menu item.
+
+    Returns:
+        tuple: A tuple of the item's store_id, id, isAvailable, price_min, price_max, price_default, and avg_calories.  If any of these values are missing, return None.
+    """
+    
+    item_id = item.get('id')
+    is_available = item.get('isAvailable')
+    price = item.get('price', None)
+
+    if price == None:
+        return None
+    
+    price_min = price.get('min')
+    price_max = price.get('max')
+    price_default = price.get('default')
+
+    if price_min * price_max * price_default == 0:
+        return None
+
+    calories = item.get('calories', None)
+
+    if calories == None or calories == 0:
+        return None
+    
+    avg_calories = (calories.get('min') + calories.get('max')) / 2
+
+    result = (store_id, item_id, is_available, price_min, price_max, price_default, avg_calories)
+
+    return result
+
+
+def simple_menu(store_id, menu):
+    """
+    Return a list of simple_menu_item tuples for each item in the menu.
+
+    Args:
+        menu (list): A list of dictionaries representing menu items.
+
+    Returns:
+        list: A list of simple_menu_item tuples.
+    """
+
+    result = [simple_menu_item(store_id, item) for item in menu]
+
+    return [item for item in result if item is not None]
+
+
+def simple_restaurant(restaurant):
+    """
+    Extracts selected information from a restaurant object.
+
+    Args:
+        restaurant (dict): A dictionary representing a restaurant.
+
+    Returns:
+        tuple: A tuple of the restaurant's id, storeId, city, state, postalCode, latitude, longitude,
+               status, hasBreakfast, hasDelivery, hasDineIn, hasDriveThru, hasMobileOrdering, hasTakeOut,
+               posVendor, and total_weekly_hours.
+               If any of these values are missing, return None for that value.
+    """
+    restaurant_id = restaurant.get('id')
+    store_id = restaurant.get('storeId')
+    address = restaurant.get('physicalAddress', {})
+    city = address.get('city')
+    state = address.get('stateProvince')
+    postal_code = address.get('postalCode')
+    latitude = restaurant.get('latitude')
+    longitude = restaurant.get('longitude')
+    status = restaurant.get('status')
+    has_breakfast = restaurant.get('hasBreakfast')
+    has_delivery = restaurant.get('hasDelivery')
+    has_dine_in = restaurant.get('hasDineIn')
+    has_drive_thru = restaurant.get('hasDriveThru')
+    has_mobile_ordering = restaurant.get('hasMobileOrdering')
+    has_take_out = restaurant.get('hasTakeOut')
+    pos_vendor = restaurant.get('posVendor')
+
+    # Calculate total weekly hours
+    total_weekly_hours = 0
+    for day in ['mon', 'tue', 'wed', 'thr', 'fri', 'sat', 'sun']:
+        open_time = restaurant.get('diningRoomHours', {}).get(f'{day}Open')
+        close_time = restaurant.get('diningRoomHours', {}).get(f'{day}Close')
+        if open_time and close_time:
+            open_hour, open_minute, _ = map(int, open_time.split(':'))
+            close_hour, close_minute, _ = map(int, close_time.split(':'))
+            total_weekly_hours += (close_hour * 60 + close_minute - open_hour * 60 - open_minute) / 60
+
+    result = (restaurant_id, store_id, city, state, postal_code, latitude, longitude, status,
+              has_breakfast, has_delivery, has_dine_in, has_drive_thru, has_mobile_ordering, has_take_out,
+              pos_vendor, total_weekly_hours)
+
+    return result
+
+
+def simple_restaurants(restaurants):
+    """
+    Return a list of simple_restaurant tuples for each restaurant in the list.
+
+    Args:
+        restaurants (list): A list of dictionaries representing restaurants.
+
+    Returns:
+        list: A list of simple_restaurant tuples.
+    """
+
+    result = [simple_restaurant(restaurant) for restaurant in restaurants]
+
+    return [restaurant for restaurant in result if restaurant is not None]
+
+
+def write_menu_items_to_csv(store_ids):
+    """
+    Use the BKClient to get the menu items for the given store ids and write the menu items to a CSV file.
+
+    Args:
+        store_ids (list): A list of store ids.
+        file_name (str): The name of the CSV file to write the menu items to.
+
+    Returns:
+        item_ids (set): A set of item ids.
+    """
+
+    save_prefix = time.strftime("%Y-%m-%d-")
+
+    all_item_ids = set()
+
+    created_date = time.strftime("%Y-%m-%d")
+
+    with open(f'Temp{os.sep}{save_prefix}bk_data.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['store_id', 'item_id', 'isAvailable', 'price_min', 'price_max', 'price_default', 'avg_calories', 'created_date'])
+
+        batch_increment = 100
+        cur, end = 0, batch_increment
+        while cur < len(store_ids):
+            print(f"Starting {cur} of {len(store_ids)}")
+            cur_ids = store_ids[cur:end]
+            all_menus = bkc.get_many_menus(cur_ids, threads=10)
+
+            # Save the data to a CSV file
+            rows = []
+            for store_id, menu in all_menus.items():
+                rows += simple_menu(store_id, menu)
+            
+            # append created_date to each row
+            for i in range(len(rows)):
+                rows[i] = rows[i] + (created_date,)
+            writer.writerows(rows)
+
+            cur += batch_increment
+            end += batch_increment
+
+            # keep track of item ids
+            for row in rows:
+                all_item_ids.add(row[1])
+
+            print(f"Finished {min(cur, len(store_ids))} of {len(store_ids)}")
+
+    return all_item_ids
+
+
+def whole_harvest():
+    stores = search_usa()
+    store_ids = list(stores.keys())
+    # prefix for current date YYYY-MM-DD-
+    save_prefix = time.strftime("%Y-%m-%d-")
+
+    with open(f'Temp{os.sep}{save_prefix}bk_restaurants.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['restaurant_id', 'store_id', 'city', 'state', 'postal_code', 'latitude', 'longitude', 'status', 'has_breakfast', 'has_delivery', 'has_dine_in', 'has_drive_thru', 'has_mobile_ordering', 'has_take_out', 'pos_vendor', 'total_weekly_hours'])
+
+        rows = simple_restaurants(stores.values())
+
+        writer.writerows(rows)
+    stores = None
+
+    all_item_ids = write_menu_items_to_csv(store_ids)
+
+    print("Finished all stores")
+
+    all_item_infos = bkc.get_many_item_info(list(all_item_ids), threads=10)
+
+    with open(f'Temp{os.sep}{save_prefix}bk_items.csv', 'w', newline='') as file:
+        # ItemInfo(id='picker_5520', name='Whopper', image_url='https://cdn.sanity.io/images/czqk28jt/prod_bk_us/e8dfc0b5c84670d64195a6602ef7f99eb70fe764-1333x1333.png', nutrition={'calories': 485.215, 'fat': 21.705, 'saturatedFat': 9, 'transFat': 0, 'cholesterol': 70, 'sodium': 583.475, 'carbohydrates': 46.92, 'fiber': 1.975, 'sugar': 8.63, 'proteins': 30.5}, is_dummy=False)
+        writer = csv.writer(file)
+
+        writer.writerow(['item_id', 'name', 'image_url', 'calories', 'fat', 'saturatedFat', 'transFat', 'cholesterol', 'sodium', 'carbohydrates', 'fiber', 'sugar', 'proteins', 'is_dummy', 'category'])
+
+        rows = []
+        for item_id, item_info in all_item_infos.items():
+            row = (item_id, item_info.name, item_info.image_url, item_info.nutrition['calories'], item_info.nutrition['fat'], item_info.nutrition['saturatedFat'], item_info.nutrition['transFat'], item_info.nutrition['cholesterol'], item_info.nutrition['sodium'], item_info.nutrition['carbohydrates'], item_info.nutrition['fiber'], item_info.nutrition['sugar'], item_info.nutrition['proteins'], item_info.is_dummy, item_info.category)
+            rows.append(row)
+
+        writer.writerows(rows)
+
+    print("Finished all items")
+
+
+
+def simple_restaurant(restaurant):
+    """
+    Extracts selected information from a restaurant object.
+
+    Args:
+        restaurant (dict): A dictionary representing a restaurant.
+
+    Returns:
+        tuple: A tuple of the restaurant's id, storeId, city, state, postalCode, latitude, longitude,
+               status, hasBreakfast, hasDelivery, hasDineIn, hasDriveThru, hasMobileOrdering, hasTakeOut,
+               posVendor, and total_weekly_hours.
+               If any of these values are missing, return None for that value.
+    """
+    restaurant_id = restaurant.get('id')
+    store_id = restaurant.get('storeId')
+    address = restaurant.get('physicalAddress', {})
+    city = address.get('city')
+    state = address.get('stateProvince')
+    postal_code = address.get('postalCode')
+    latitude = restaurant.get('latitude')
+    longitude = restaurant.get('longitude')
+    status = restaurant.get('status')
+    has_breakfast = restaurant.get('hasBreakfast')
+    has_delivery = restaurant.get('hasDelivery')
+    has_dine_in = restaurant.get('hasDineIn')
+    has_drive_thru = restaurant.get('hasDriveThru')
+    has_mobile_ordering = restaurant.get('hasMobileOrdering')
+    has_take_out = restaurant.get('hasTakeOut')
+    pos_vendor = restaurant.get('posVendor')
+
+    # Calculate total weekly hours
+    total_weekly_hours = 0
+    for day in ['mon', 'tue', 'wed', 'thr', 'fri', 'sat', 'sun']:
+        open_time = restaurant.get('diningRoomHours', {}).get(f'{day}Open')
+        close_time = restaurant.get('diningRoomHours', {}).get(f'{day}Close')
+        if open_time and close_time:
+            open_hour, open_minute, _ = map(int, open_time.split(':'))
+            close_hour, close_minute, _ = map(int, close_time.split(':'))
+            total_weekly_hours += (close_hour * 60 + close_minute - open_hour * 60 - open_minute) / 60
+
+    result = (restaurant_id, store_id, city, state, postal_code, latitude, longitude, status,
+              has_breakfast, has_delivery, has_dine_in, has_drive_thru, has_mobile_ordering, has_take_out,
+              pos_vendor, total_weekly_hours)
+
+    return result
+
+
+def simple_restaurants(restaurants):
+    """
+    Return a list of simple_restaurant tuples for each restaurant in the list.
+
+    Args:
+        restaurants (list): A list of dictionaries representing restaurants.
+
+    Returns:
+        list: A list of simple_restaurant tuples.
+    """
+
+    result = [simple_restaurant(restaurant) for restaurant in restaurants]
+
+    return [restaurant for restaurant in result if restaurant is not None]
+
+
+def menu_items_update():
+    """
+    Find the most recent restaurants list in the Temp folder.  Use the store ids to get the most recent menu items from the BK API.
+    Save the menu items to a new CSV file in the Temp folder.
+    """
+
+    # Find the most recent restaurants list in the Temp folder
+    files = os.listdir('Temp')
+    files = [f for f in files if 'bk_restaurants' in f and f.endswith('.csv')]
+    files.sort(reverse=True)
+    if len(files) == 0:
+        print("No restaurants files found")
+        return
+    restaurants_file = files[0]
+
+    # Read the store ids from the restaurants file
+    store_ids = set()
+    with open(f'Temp{os.sep}{restaurants_file}', 'r', newline='') as file:
+        reader = csv.reader(file)
+        next(reader)
+        for row in reader:
+            store_ids.add(row[1])
+
+
+    write_menu_items_to_csv(list(store_ids))
+
+
+
+
+menu_items_update()
